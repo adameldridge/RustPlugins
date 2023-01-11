f2271ebc838ff404fc39a24c6c94dea9470df8e5
Adam Eldridge <adameldridge89@gmail.com>
2023-01-11 22:08:43 +0000

    Add project files.

---
+++ Test.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Oxide.Plugins
+{
+    [Info("Adam Test", "Test", "1.0.0")]
+    [Description("Test Plugin")]
+
+    public class Test : RustPlugin
+    {
+        void OnWeaponFired(BaseProjectile projectile, BasePlayer player, ItemModProjectile mod, ProtoBuf.ProjectileShoot projectiles)
+        {
+            Puts("OnWeaponFired works!");
+            player.ChatMessage("You fired a gun!!");
+        }
+    }
+}
